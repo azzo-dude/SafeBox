3cd890c0af4398337cc22f94f903101dafe58c2c
Nguyễn Quý Ngọc <76089776+azzo-dude@users.noreply.github.com>
2025-06-24 13:25:22 +0700

    Update Components.h

--- Components.h
+++ Components.h
@@ -1,1 +1,36 @@
+#ifndef COMPONENTS_H
+#define COMPONENTS_H
 
+class Led {
+  private:
+    uint8_t  _pin;
+
+  public:
+    Led(uint8_t pin) : _pin(pin) {
+      pinMode(_pin, OUTPUT);
+      digitalWrite(_pin, LOW);
+    }
+}
+
+class Keypad3x4 {
+}
+
+class Buzzer {
+
+}
+
+class Lcd16x2I2C {
+
+}
+
+class Nfc {
+
+}
+
+class FingerprintSensor {
+
+}
+
+class
+
+#endif
